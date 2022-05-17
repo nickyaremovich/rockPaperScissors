f72e24252785d1f64e8273193091a0538691cca3
Nick Yaremovich <nickyaremovich@gmail.com>
2022-05-17 15:59:54 -0600

    text alignment

---
+++ script.js
@@ -0,0 +1,26 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const humanChoiceDisplay = document.getElementById("human-choice");
+const resultDisplay = document.getElementById("result");
+const choices = document.querySelectorAll("button");
+
+choices.forEach(choice => choice.addEventListener('click', (ev) => {
+    humanChoice = ev.target.id
+    humanChoiceDisplay.innerHTML = humanChoice
+    genComputerChoice()
+}))
+
+function genComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * choices.length + 1)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
