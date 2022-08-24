122d5b266ae8643cd9c666d1c19e430e7b3a0ab8
Nick Yaremovich <nickyaremovich@gmail.com>
2022-08-24 17:37:09 -0600

    added event listener for buttons

--- script.js
+++ script.js
@@ -1,53 +1,15 @@
 // variables 
-const choices = ['rock', 'paper', 'scissors'];
-let round = 0;
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissors = document.querySelector('.scissors');
-const humanChoice = document.querySelector('p.humanChoice');
-const compChoice = document.getElementsByClassName('.compChoice');
+const humanChoiceDisplay = document.getElementById('human-choice');
+const compChoiceDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById("result");
+const choices = document.querySelectorAll('button');
+let humanChoice;
 
-// comp choice functiom
-function compGuess() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
+choices.forEach(choice => choice.addEventListener('click', (e) => {
+    humanChoice = e.target.id;
+    humanChoiceDisplay.innerHTML = humanChoice;
+    compChoice();
+}))
 
-// play round 
-function playRound(playerSelection, compSelection) {
-    if (playerSelection === compSelection) {
-        return "it's a tie!";
-    } else if (playerSelection === 'rock' && compSelection === 'scissors') {
-        return "human wins!";
-    } else if (playerSelection === 'paper' && compSelection === 'rock') {
-        return "player wins";
-    } else if (playerSelection === 'scissors' && compSelection === 'paper') {
-        return "scissors beats rock! player wins";
-    } else if (playerSelection === 'rock' && compSelection === 'paper') {
-        return "paper beats rock, player loses";
-    } else if (playerSelection === 'paper' && compSelection === 'scissors') {
-        return "computer wins";
-    } else if (playerSelection === 'scissors' && compSelection === 'rock') {
-        return "computer wins";
-    }
- }
-function playGame() {
-    playRound();
-    round++;
-}
 
- // user input function
-rock.addEventListener('click', () => {
-    humanChoice.innerHTML = 'Your Choice: You chose rock';
-    playRound();
-})
 
-paper.addEventListener('click', () => {
-    humanChoice.innerHTML = "Your Choice: You chose paper";
-    playRound();
-})
-
-scissors.addEventListener('click', () => {
-    humanChoice.innerHTML = "Your Choice: You chose scissors";
-    playRound();
-})
-playGame();
